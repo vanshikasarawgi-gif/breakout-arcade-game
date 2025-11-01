914a61b2a3791d5f933007057a9844a186761e97
Vanshika Sarawgi <vanshikasarawgi@gmail.com>
2025-11-01 12:33:08 +0530

    feat: add keyboard controls to paddle to move it left or right

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ screen.title("Breakout Arcade Game")
 #create paddle
 paddle = Paddle(0,-200)
 
+#key control
+screen.listen()
+screen.onkey(paddle.right,key="d")
+screen.onkey(paddle.left,key="a")
+screen.onkey(paddle.right,key="Right")
+screen.onkey(paddle.left,key="Left")
 
 
 
